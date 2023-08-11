392a7438b542a4d91de2ec68bde92f7c1dcd2a20
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-08-11 13:50:44 +0200

    add problems

--- Public/Get-MyMailboxProblems.ps1
+++ Public/Get-MyMailboxProblems.ps1
@@ -16,6 +16,7 @@
             NoDatabase                  = [System.Collections.Generic.List[PSCustomObject]]::new()
             ContactMissingPrimarySmtp   = [System.Collections.Generic.List[PSCustomObject]]::new()
             ContactMissingExternalEmail = [System.Collections.Generic.List[PSCustomObject]]::new()
+            MissingUserPrincipalName    = [System.Collections.Generic.List[PSCustomObject]]::new()
         }
         Local  = [ordered] @{
             BrokenDisplayName           = [System.Collections.Generic.List[PSCustomObject]]::new()
@@ -23,6 +24,7 @@
             NoDatabase                  = [System.Collections.Generic.List[PSCustomObject]]::new()
             ContactMissingPrimarySmtp   = [System.Collections.Generic.List[PSCustomObject]]::new()
             ContactMissingExternalEmail = [System.Collections.Generic.List[PSCustomObject]]::new()
+            MissingUserPrincipalName    = [System.Collections.Generic.List[PSCustomObject]]::new()
         }
         Both   = [ordered] @{
             DuplicateAlias   = [System.Collections.Generic.List[PSCustomObject]]::new()
@@ -31,17 +33,34 @@
     }
 
     $PropertiesMailbox = @(
-        'Identity', 'UserPrincipalName', 'Alias', 'DisplayName', 'Database', 'PrimarySmtpAddress', 'RecipientType', 'RecipientTypeDetails', 'ExchangeUserAccountControl'
+        'UserPrincipalName', 'Alias', 'DisplayName', 'Database', 'PrimarySmtpAddress', 'RecipientType', 'RecipientTypeDetails', 'ExchangeUserAccountControl' #, 'Identity'
     )
-
     if ($Local) {
-        $ContactsLocal = Get-LocalMailContact -ResultSize Unlimited
-        $MailboxesLocal = Get-LocalMailbox -ResultSize Unlimited | Select-Object -Property $PropertiesMailbox
+        try {
+            $ContactsLocal = Get-LocalMailContact -ResultSize Unlimited -ErrorAction Stop
+        } catch {
+            Write-Warning "Get-MyMailboxProblems - Unable to get local contacts: $($_.Exception.Message)"
+            $ContactsLocal = @()
+        }
+        try {
+            $MailboxesLocal = Get-LocalMailbox -ResultSize Unlimited | Select-Object -Property $PropertiesMailbox -ErrorAction Stop
+        } catch {
+            Write-Warning "Get-MyMailboxProblems - Unable to get local mailboxes: $($_.Exception.Message)"
+            $MailboxesLocal = @()
+        }
     }
-
-    $ContactsOnline = Get-LocalMailContact -ResultSize Unlimited
-    $MailboxesOnline = Get-EXOMailbox -ResultSize Unlimited -Properties $PropertiesMailbox | Select-Object -Property $PropertiesMailbox
-
+    try {
+        $ContactsOnline = Get-LocalMailContact -ResultSize Unlimited -ErrorAction Stop
+    } catch {
+        Write-Warning "Get-MyMailboxProblems - Unable to get online contacts: $($_.Exception.Message)"
+        $ContactsOnline = @()
+    }
+    try {
+        $MailboxesOnline = Get-EXOMailbox -ResultSize Unlimited -Properties $PropertiesMailbox | Select-Object -Property $PropertiesMailbox -ErrorAction Stop
+    } catch {
+        Write-Warning "Get-MyMailboxProblems - Unable to get online mailboxes: $($_.Exception.Message)"
+        $MailboxesOnline = @()
+    }
     foreach ($C in $ContactsLocal) {
         if (-not $C.PrimarySmtpAddress) {
             $Problems.Local.ContactMissingPrimarySmtp.Add($C)
@@ -66,6 +85,9 @@
         if ($Null -eq $M.Database) {
             $Problems.Local.NoDatabase.Add($M)
         }
+        if ($null -eq $M.UserPrincipalName) {
+            $Problems.Local.MissingUserPrincipalName.Add($M)
+        }
     }
     foreach ($M in $MailboxesOnline) {
         if ($M.DisplayName.StartsWith(' ') -or $M.DisplayName.EndsWith(' ')) {
@@ -73,6 +95,9 @@
         }
         if ($Null -eq $M.Database) {
             $Problems.Online.NoDatabase.Add($M)
+        }
+        if ($null -eq $M.UserPrincipalName) {
+            $Problems.Local.MissingUserPrincipalName.Add($M)
         }
     }
 
@@ -114,16 +139,24 @@
     $Cache = [ordered] @{}
     $CacheUPN = [ordered] @{}
     $CacheAllUPN = [ordered] @{}
-    foreach ($M in $MailboxesOnline + $MailboxesLocal) {
+    $Mailboxes = @(
+        if ($MailboxesOnline.Count -gt 0) {
+            $MailboxesOnline
+        }
+        if ($MailboxesLocal.Count -gt 0) {
+            $MailboxesLocal
+        }
+    )
+    foreach ($M in $Mailboxes) {
         # Duplicate Accounts
-        if (-not $CacheUPN[$M.UserPrincipalName]) {
-            $CacheUPN[$M.UserPrincipalName] = $M
+        if (-not $CacheUPN[$M.PrimarySmtpAddress]) {
+            $CacheUPN[$M.PrimarySmtpAddress] = $M
         } else {
-            if (-not $CacheAllUPN[$M.UserPrincipalName]) {
-                $CacheAllUPN[$M.UserPrincipalName] = [System.Collections.Generic.List[PSCustomObject]]::new()
-                $CacheAllUPN[$M.UserPrincipalName].Add($CacheUPN[$M.UserPrincipalName])
+            if (-not $CacheAllUPN[$M.PrimarySmtpAddress]) {
+                $CacheAllUPN[$M.PrimarySmtpAddress] = [System.Collections.Generic.List[PSCustomObject]]::new()
+                $CacheAllUPN[$M.PrimarySmtpAddress].Add($CacheUPN[$M.PrimarySmtpAddress])
             }
-            $CacheAllUPN[$M.UserPrincipalName].Add($M)
+            $CacheAllUPN[$M.PrimarySmtpAddress].Add($M)
         }
         # Duplicate Aliases
         if (-not $Cache[$M.Alias]) {
