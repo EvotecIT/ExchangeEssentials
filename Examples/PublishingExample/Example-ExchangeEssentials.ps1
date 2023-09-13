d26b8a2e90604a72f15e558a571b744eb511c55d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-13 09:27:28 +0200

    update example

---
+++ Examples/PublishingExample/Example-ExchangeEssentials.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module $PSScriptRoot\Modules\ExchangeOnlineManagement\ExchangeOnlineManagement.psd1 -Force
+Import-Module $PSScriptRoot\Modules\ExchangeEssentials\ExchangeEssentials.psd1 -Force
+
+Get-MyMailbox -Verbose
