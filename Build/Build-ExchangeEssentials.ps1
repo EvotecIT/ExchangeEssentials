ab7f0be39268f11b453fd9ff6135520666fedc16
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-28 19:45:59 +0200

    update configuraiton

--- Build/Build-ExchangeEssentials.ps1
+++ Build/Build-ExchangeEssentials.ps1
@@ -114,6 +114,6 @@ Invoke-ModuleBuild -ModuleName 'ExchangeEssentials' {
     New-ConfigurationArtefact @newConfigurationArtefactSplat -CopyFilesRelative
 
     # global options for publishing to github/psgallery
-    New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
-    New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
+    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
+    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
 }
