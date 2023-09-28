9e73ee3e46b4834ebcee468683908824aeab8241
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-28 19:37:17 +0200

    Update

--- Private/Configuration.ReportMailboxProblems.ps1
+++ Private/Configuration.ReportMailboxProblems.ps1
@@ -18,10 +18,10 @@
             New-HTMLTabPanel -Theme forge {
                 New-HTMLTab -Name "Duplicate Alias" {
                     New-HTMLSection -HeaderText 'Exchange Online - Duplicate Alias' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Online Mailbox']['DuplicateAlias'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Online']['DuplicateAlias'] -Filtering
                     }
                     New-HTMLSection -HeaderText 'Exchange On-Premises - Duplicate Alias' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['On-Premises Mailbox']['DuplicateAlias'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Local']['DuplicateAlias'] -Filtering
                     }
                     New-HTMLSection -HeaderText 'Exchange - Duplicate Alias (Together)' {
                         New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Both']['DuplicateAlias'] -Filtering
@@ -34,32 +34,32 @@
                 }
                 New-HTMLTab -Name 'No Database' {
                     New-HTMLSection -HeaderText 'Exchange On-Premises - No Database' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['On-Premises Mailbox']['NoDatabase'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Local']['NoDatabase'] -Filtering
                     }
                 }
                 New-HTMLTab -Name 'Inconsistent Data' {
                     New-HTMLSection -HeaderText 'Exchange Online - Broken DisplayName' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Online Mailbox']['BrokenDisplayName'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Online']['BrokenDisplayName'] -Filtering
                     }
                     New-HTMLSection -HeaderText 'Exchange On-Premises - Broken DisplayName' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['On-Premises Mailbox']['BrokenDisplayName'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Local']['BrokenDisplayName'] -Filtering
                     }
                     New-HTMLSection -HeaderText 'Exchange On-Premises - Missing UserPrincipalName' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['On-Premises Mailbox']['MissingUserPrincipalName'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Local']['MissingUserPrincipalName'] -Filtering
                     }
                 }
                 New-HTMLTab -Name 'Contact Problems' {
                     New-HTMLSection -HeaderText 'Exchange Online - Contact Missing PrimarySmtp' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Online Mailbox']['ContactMissingPrimarySmtp'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Online']['ContactMissingPrimarySmtp'] -Filtering
                     }
                     New-HTMLSection -HeaderText 'Exchange On-Premises - Contact Missing PrimarySmtp' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['On-Premises Mailbox']['ContactMissingPrimarySmtp'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Local']['ContactMissingPrimarySmtp'] -Filtering
                     }
                     New-HTMLSection -HeaderText 'Exchange Online - Contact Missing ExternalEmail' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Online Mailbox']['ContactMissingExternalEmail'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Online']['ContactMissingExternalEmail'] -Filtering
                     }
                     New-HTMLSection -HeaderText 'Exchange On-Premises - Contact Missing ExternalEmail' {
-                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['On-Premises Mailbox']['ContactMissingExternalEmail'] -Filtering
+                        New-HTMLTable -DataTable $Script:Reporting['MailboxProblems']['Data']['Local']['ContactMissingExternalEmail'] -Filtering
                     }
                 }
             }
