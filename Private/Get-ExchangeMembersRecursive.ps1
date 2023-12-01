3728cbe3f5b9ba44225330232e63e28de939fbc0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-01 12:15:39 +0100

    Improvements

--- Private/Get-ExchangeMembersRecursive.ps1
+++ Private/Get-ExchangeMembersRecursive.ps1
@@ -5,42 +5,57 @@
         [Parameter(Mandatory = $true)] [ValidateSet('FullAccess', 'SendAs', 'SendOnBehalf')] [string[]] $AccessRights,
         [switch] $Local
     )
+    Begin {
+        $IgnoreAccounts = @(
+            'Everyone'
+            'NT AUTHORITY\Authenticated Users'
+        )
+    }
     Process {
-        if ($Local) {
-            $Members = try {
-                Get-LocalDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
-            } catch {
-                try {
-                    Get-LocalDynamicDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
-                } catch {
-                    Write-Warning -Message "Get-ExchangeMembersRecursive - Identity '$Identity' is not a valid group."
-                }
-            }
+        $MatchRegex = [Regex]::Matches($Identity, "S-\d-\d+-(\d+-|){1,14}\d+")
+        if ($MatchRegex.Success) {
+            # do nothing
+        } elseif ($Identity -in $IgnoreAccounts) {
+            # do nothing
         } else {
-            $Members = try {
-                Get-DistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
-            } catch {
-                try {
-                    Get-DynamicDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
+            if ($Local) {
+                $Members = try {
+                    Get-LocalDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
                 } catch {
-                    Write-Warning -Message "Get-ExchangeMembersRecursive - Identity '$Identity' is not a valid group."
-                }
-            }
-        }
-        foreach ($Member in $Members) {
-            switch ($Member) {
-                { $_.RecipientType -notlike "*Group*" } {
-                    [PSCustomObject]@{
-                        Identity          = $_.Identity
-                        Trustee           = $_.PrimarySmtpAddress
-                        AccessControlType = 'Allow'
-                        AccessRights      = @($AccessRights)
-                        IsInherited       = $false
-                        InheritanceType   = 'None'
+                    try {
+                        Get-LocalDynamicDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
+                    } catch {
+                        Write-Warning -Message "Get-ExchangeMembersRecursive - Identity '$Identity' is not a valid group."
                     }
                 }
-                Default {
-                    Get-ExchangeMembersRecursive -Identity $_.primarysmtpAddress -Local:$Local.IsPresent -AccessRights $AccessRights
+            } else {
+                $Members = try {
+                    Get-DistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
+                } catch {
+                    try {
+                        Get-DynamicDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
+                    } catch {
+                        Write-Warning -Message "Get-ExchangeMembersRecursive - Identity '$Identity' is not a valid group."
+                    }
+                }
+            }
+            foreach ($Member in $Members) {
+                switch ($Member) {
+                    { $_.RecipientType -notlike "*Group*" } {
+                        [PSCustomObject]@{
+                            Identity          = $_.Identity
+                            Trustee           = $_.PrimarySmtpAddress
+                            AccessControlType = 'Allow'
+                            AccessRights      = @($AccessRights)
+                            IsInherited       = $false
+                            InheritanceType   = 'None'
+                        }
+                    }
+                    Default {
+                        if ($_.primarysmtpAddress) {
+                            Get-ExchangeMembersRecursive -Identity $_.primarysmtpAddress -Local:$Local.IsPresent -AccessRights $AccessRights
+                        }
+                    }
                 }
             }
         }
