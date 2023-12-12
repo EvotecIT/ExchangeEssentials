a06067a758e157289146c57b7e204be232460806
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-12 15:11:03 +0100

    Add ConvertTo-ExchangeAccessRights function

---
+++ Private/ConvertTo-ExchangeAccessRights.ps1
@@ -0,0 +1,18 @@
+﻿function ConvertTo-ExchangeAccessRights {
+    [CmdletBinding()]
+    Param (
+        [parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)][PSObject] $Object,
+        [string] $Identity,
+        [string] $AccessRights = 'FullAccess'
+    )
+    Process {
+        [PSCustomObject]@{
+            Identity          = $Identity
+            Trustee           = $Object.PrimarySmtpAddress
+            AccessControlType = 'Allow'
+            AccessRights      = @($AccessRights)
+            IsInherited       = $false
+            InheritanceType   = 'None'
+        }
+    }
+}
