2e57130ac41d3e57076ed550ffdf2dc860456b38
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-12 15:11:28 +0100

    Add Get-MyMailboxMembers function to retrieve mailbox members recursively

--- Public/Get-MyMailboxMembers.ps1
+++ Public/Get-MyMailboxMembers.ps1
@@ -1,23 +1,39 @@
-﻿function Get-ExchangeMembersRecursive {
+﻿function Get-MyMailboxMembers {
+    [alias('Get-ExchangeMembersRecursive', 'Get-MyMailboxMembersRecursive')]
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)] $Identity,
-        [Parameter(Mandatory = $true)] [ValidateSet('FullAccess', 'SendAs', 'SendOnBehalf')] [string[]] $AccessRights,
-        [switch] $Local
+        [Parameter(Mandatory, ValueFromPipeline)] $Identity,
+        [switch] $Local,
+        [Parameter(DontShow)][int] $Nesting = -1,
+        [Parameter(DontShow)][switch] $Nested
     )
     Begin {
         $IgnoreAccounts = @(
             'Everyone'
             'NT AUTHORITY\Authenticated Users'
+            "Discovery Management"
         )
+        if ($Nesting -eq -1) {
+            $GroupCache = [ordered] @{}
+        }
     }
     Process {
+        $Nesting++
         $MatchRegex = [Regex]::Matches($Identity, "S-\d-\d+-(\d+-|){1,14}\d+")
         if ($MatchRegex.Success) {
             # do nothing
         } elseif ($Identity -in $IgnoreAccounts) {
             # do nothing
         } else {
+            if (-not $GroupCache[$Identity]) {
+                $GroupCache[$Identity] = $true
+            } else {
+                Write-Verbose -Message "Get-MyMailboxMembers - Identity '$Identity' already processed. Circular reference detected."
+                return
+                # Write-Color "Identity '$Identity' already processed." -Color Red
+                #return
+            }
+            Write-Verbose -Message "Processing '$Identity'"
             if ($Local) {
                 $Members = try {
                     Get-LocalDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
@@ -25,7 +41,7 @@
                     try {
                         Get-LocalDynamicDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
                     } catch {
-                        Write-Warning -Message "Get-ExchangeMembersRecursive - Identity '$Identity' is not a valid group."
+                        Write-Warning -Message "Get-MyMailboxMembers - Identity '$Identity' is not a valid group."
                     }
                 }
             } else {
@@ -35,29 +51,28 @@
                     try {
                         Get-DynamicDistributionGroupMember -Identity $Identity -ResultSize Unlimited -ErrorAction Stop
                     } catch {
-                        Write-Warning -Message "Get-ExchangeMembersRecursive - Identity '$Identity' is not a valid group."
+                        Write-Warning -Message "Get-MyMailboxMembers - Identity '$Identity' is not a valid group."
                     }
                 }
             }
             foreach ($Member in $Members) {
                 switch ($Member) {
                     { $_.RecipientType -notlike "*Group*" } {
-                        [PSCustomObject]@{
-                            Identity          = $_.Identity
-                            Trustee           = $_.PrimarySmtpAddress
-                            AccessControlType = 'Allow'
-                            AccessRights      = @($AccessRights)
-                            IsInherited       = $false
-                            InheritanceType   = 'None'
-                        }
+                        $_
                     }
                     Default {
                         if ($_.primarysmtpAddress) {
-                            Get-ExchangeMembersRecursive -Identity $_.primarysmtpAddress -Local:$Local.IsPresent -AccessRights $AccessRights
+                            Get-MyMailboxMembers -Identity $_.primarysmtpAddress -Local:$Local.IsPresent -Nested -Nesting $Nesting
                         }
                     }
                 }
             }
+        }
+    }
+    End {
+        if (-not $Nested) {
+            #Write-Color -Text "Get-MyMailboxMembers - Clearing GroupCache" -Color Red
+            $GroupCache = $Null
         }
     }
 }
